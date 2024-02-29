a3b22defdcd45b3c327d7470ca477817a008d8d8
Sundardasps <sundardasps0055@gmail.com>
2024-02-29 14:40:41 +0530

    ui changed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function clickButton(name) {
-  let div = document.getElementById(`tag-${name}`);
-  div.style.backgroundColor =
-    div.style.backgroundColor === name ? "transparent" : name;
+  let div = document.getElementById("tag");
+  div.style.backgroundColor = div.style.backgroundColor === name ? "transparent" : name;
+  div.innerText = name + "Tag";
 }
